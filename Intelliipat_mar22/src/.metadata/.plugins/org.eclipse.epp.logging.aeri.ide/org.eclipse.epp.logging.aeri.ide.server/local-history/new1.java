73fd3b9c2eedd32bf91ecc790b863774c607171f
Anjana Nayyar <49352963+anayyar26191@users.noreply.github.com>
2020-05-09 17:26:17 -0400

    commit
    commit

---
+++ Intelliipat_mar22/src/.metadata/.plugins/org.eclipse.epp.logging.aeri.ide/org.eclipse.epp.logging.aeri.ide.server/local-history/new1.java
@@ -0,0 +1,20 @@
+
+javac
+javacjava_Home 
+environment variables in system variables
+Java_Home click new
+PATH -edit or new
+cmd\java -version
+class name TestWorld
+Attribue name: regNum,fileValue
+method name
+public static void main(String[] args){
+	System.out.println("HelloWorld")
+
+}
+cmd
+set CLASSPATH=%CLASSPATH%
+C:\Program Files\Java\jdk1.8.0_201\jre|bin
+C:\Program Files\Java\jdk1.8.0_201
+
+
